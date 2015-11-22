87c98fa069c9cb366eff1e42ac3370b1d2d1ec91
Emil <emilmoham@gmail.com>
2015-11-22 15:49:23 -0500

    Added Player field. Added function addPlayer to set the player field. Added BoxCollider Array List field. Changed addGroundObject function to add a BoxCollider ojbect on creation of a GroundObject.

--- src/simpleguiengine/OverWorld.java
+++ src/simpleguiengine/OverWorld.java
@@ -7,22 +7,35 @@ import simplegui.SimpleGUI;
 public class OverWorld {
     double floor;
     ArrayList<GroundObject> GroundObjects = new ArrayList();
+    ArrayList<BoxCollider> BoxColliders = new ArrayList();
     SimpleGUI globe;
+    Player p1 = null;
     
     public OverWorld(SimpleGUI sg, double baseElevation){
         floor = baseElevation;
         globe = sg;
         globe.setBackgroundColor(Color.BLUE);
+        
     }
     
     public void addGroundObject(GroundObject g){
         GroundObjects.add(g);
+        BoxColliders.add(g.b);
+    }
+    
+    public void addPlayer(Player p){
+        p1 = p;
     }
     
     public void visualizeAll(){
         for(int i = 0; i < GroundObjects.size(); i++){
             GroundObjects.get(i).visualize(globe);
         }
+        
+        if(p1 != null){
+            p1.visualize();
+        }
+        
         globe.drawFilledBox(0,globe.getHeight()-floor, globe.getWidth(), floor, Color.DARK_GRAY, 1.0, "floor");
     }
 }
