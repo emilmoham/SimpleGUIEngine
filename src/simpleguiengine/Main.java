9d5be0cad3e42aa0d0b70129f02f71416301c99e
Emil <emilmoham@gmail.com>
2015-11-22 15:51:05 -0500

    Re-enabled Player creation to test OverWorld function.

--- src/simpleguiengine/Main.java
+++ src/simpleguiengine/Main.java
@@ -1,18 +1,19 @@
 package simpleguiengine;
-
 import simplegui.SimpleGUI;
 
 public class Main {
 
     public static void main(String[] args) {
         SimpleGUI sg = new SimpleGUI();
+        
         sg.timerStart(30);
-        //Player p1 = new Player(sg);
+        Player p1 = new Player(sg);
+        p1.setX(sg.getWidth()/20);
         //World w1 = new World(p1, sg);
-        System.out.println(""+ (int)'<');
         double squareSize = 50;
         OverWorld world = new OverWorld(sg, 50);
         world.addGroundObject(new GroundObject(9*squareSize, sg.getHeight() - world.floor - squareSize, squareSize, squareSize));
+        world.addPlayer(p1);
         world.visualizeAll();
     }
 }
