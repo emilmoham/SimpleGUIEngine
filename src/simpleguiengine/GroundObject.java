30553bda9f5f130a79bae8ca521e87e3647180ea
Emil <emilmoham@gmail.com>
2015-11-23 16:38:10 -0500

    Changed constructor accomodate using it as a parent class for the Player class. BoxColliders are now initialized with their own functions in the constructor of the Ground Object class.

--- src/simpleguiengine/GroundObject.java
+++ src/simpleguiengine/GroundObject.java
@@ -4,23 +4,35 @@ import java.awt.Color;
 import simplegui.SimpleGUI;
 
 public class GroundObject {
+
     double xTopLeft;
     double yTopLeft;
     double width;
     double height;
-    
+
     BoxCollider b;
-    
-    public GroundObject(double x, double y, double w, double h){
+
+    public GroundObject(double x, double y, double w, double h) {
         xTopLeft = x;
         yTopLeft = y;
         width = w;
         height = h;
-        
-        b = new BoxCollider(x,y,w,h);
+        initBoxCollider();
     }
     
-    public void visualize(SimpleGUI sg){
+    public void setX(double x) {
+        xTopLeft = x;
+    }
+
+    public void setY(double y) {
+        yTopLeft = y;
+    }
+
+    public void initBoxCollider() {
+        b = new BoxCollider(xTopLeft, yTopLeft, width, height);
+    }
+
+    public void visualize(SimpleGUI sg) {
         sg.drawFilledBox(xTopLeft, yTopLeft, width, height, Color.red, 1.0, "GO");
     }
 }
