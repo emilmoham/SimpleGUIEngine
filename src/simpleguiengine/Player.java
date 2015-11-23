34fda1a159acf91f5094e5bdaa8568efb802507a
Emil <emilmoham@gmail.com>
2015-11-23 16:41:53 -0500

    Player Class now extends Ground Object class. This makes it easier to keep track of objects in the OverWorld Class with our GroundObject array list. Also added keyboard controls that move the player based on time.

--- src/simpleguiengine/Player.java
+++ src/simpleguiengine/Player.java
@@ -2,32 +2,78 @@ package simpleguiengine;
 
 import java.awt.Color;
 import simplegui.SimpleGUI;
+import simplegui.TimerListener;
+import simplegui.KeyboardListener;
 
-public class Player {
+public class Player extends GroundObject implements TimerListener {
 
     //Visualization Data
     SimpleGUI globe;
-    int playerSize = 50;
-    int startX;
-    int startY;
+    double playerSize = 50;
 
-    public Player(SimpleGUI sg) {
+    boolean moveLeft = false;
+    boolean moveRight = false;
+
+    KeyboardListener controls = new KeyboardListener() {
+
+        @Override
+        public void reactToKeyboardEntry(String string) {
+            //Unused
+        }
+
+        @Override
+        public void reactToKeyboardSingleKey(String string) {
+            //WSAD
+            char c = string.charAt(0);
+            /*
+             if (c == 'w') {
+             globe.print("");
+             globe.print("UP");
+             }
+             if(c == 's'){
+             globe.print("");
+             globe.print("DOWN");
+             }
+             */
+            if (c == 'a') {
+                globe.print("");
+                globe.print("LEFT");
+                moveLeft = true;
+            }
+            if (c == 'd') {
+                globe.print("");
+                globe.print("RIGHT");
+                moveRight = true;
+            }
+        }
+    };
+
+    public Player(double x, double y, double w, double h, SimpleGUI sg) {
+        super(x, y, w, h);
+        playerSize = (w+h)/2;
         globe = sg;
-        startX = globe.getWidth() / 2;
-        startY = globe.getHeight() - 100;
-    }
-    
-    public void setX(int x){
-        startX = x;
-    }
-    
-    public void setY(int y){
-        startY = y;
+        
+        globe.registerToKeyboard(controls);
+        globe.registerToTimer(this);
     }
 
     public void visualize() {
-        globe.drawFilledBox(startX, startY, playerSize, playerSize, Color.GREEN, 1.0, "Player");
+        globe.drawFilledBox(xTopLeft, yTopLeft, width, height, Color.GREEN, 1.0, "Player");
     }
-    
-    
+
+    @Override
+    public void reactToTimer(long l) {
+        if (moveRight) {
+            moveRight = false;
+            globe.animMoveAllRel(4, 0, "Player");
+            xTopLeft += 4;
+        } else if (moveLeft) {
+            moveLeft = false;
+            globe.animMoveAllRel(-4, 0, "Player");
+            yTopLeft -= 4;
+        }
+        b.updatePosition(xTopLeft, yTopLeft);
+        globe.repaintPanel();
+    }
+
 }
