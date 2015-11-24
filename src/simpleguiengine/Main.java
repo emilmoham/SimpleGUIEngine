362a54827a29cfdb893d06aa75687e06d588e7d7
Emil <emilmoham@gmail.com>
2015-11-23 19:20:32 -0500

    Updated Main class to use new Player constructor. Removed commented World Class instantiation.

--- src/simpleguiengine/Main.java
+++ src/simpleguiengine/Main.java
@@ -6,12 +6,11 @@ public class Main {
     public static void main(String[] args) {
         SimpleGUI sg = new SimpleGUI();
         
-        sg.timerStart(30);
-        Player p1 = new Player(sg);
-        p1.setX(sg.getWidth()/20);
-        //World w1 = new World(p1, sg);
+        sg.timerStart(15);
         double squareSize = 50;
+        
         OverWorld world = new OverWorld(sg, 50);
+        Player p1 = new Player(sg.getWidth()/20, sg.getHeight() - world.floor - squareSize, squareSize, squareSize, sg);
         world.addGroundObject(new GroundObject(9*squareSize, sg.getHeight() - world.floor - squareSize, squareSize, squareSize));
         world.addPlayer(p1);
         world.visualizeAll();
