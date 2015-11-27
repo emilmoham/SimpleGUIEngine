aaa6ebe25060d2b7accd927afc7dbd713442b45e
Emil <emilmoham@gmail.com>
2015-11-26 23:30:15 -0500

    Added ability to jump and fall

--- src/simpleguiengine/Player.java
+++ src/simpleguiengine/Player.java
@@ -12,11 +12,16 @@ public class Player extends GroundObject implements TimerListener {
     SimpleGUI globe;
     //double playerSize = 50;
 
+    //Physics and movement data
     boolean moveLeft = false;
     boolean moveRight = false;
-
+    boolean playerIsJumping = false;
+    double gravity = 2;
+    double jumpStrength = -20;
+    double speedY = 0;
     double speedX = 5;
     double movement;
+    double sampleRate = 0.1;
 
     public ArrayList<BoxCollider> bcs;
 
@@ -31,22 +36,25 @@ public class Player extends GroundObject implements TimerListener {
         public void reactToKeyboardSingleKey(String string) {
             //WSAD
             char c = string.charAt(0);
+            if (c == 'w') {
+                globe.print("");
+                globe.print("UP");
+                if (!playerIsJumping) {
+                    playerIsJumping = true;
+                    speedY = jumpStrength;
+                }
+            }
             /*
-             if (c == 'w') {
-             globe.print("");
-             globe.print("UP");
-             }
              if(c == 's'){
              globe.print("");
              globe.print("DOWN");
-             }
-             */
+             }*/
+
             if (c == 'a') {
                 globe.print("");
                 globe.print("LEFT");
                 moveLeft = true;
-            }
-            if (c == 'd') {
+            } else if (c == 'd') {
                 globe.print("");
                 globe.print("RIGHT");
                 moveRight = true;
@@ -58,12 +66,11 @@ public class Player extends GroundObject implements TimerListener {
         super(x, y, w, h);
         //playerSize = (w+h)/2;
         globe = sg;
-
         globe.registerToKeyboard(controls);
         globe.registerToTimer(this);
     }
 
-    public double checkDistanceX(double desiredMovement) {
+    double checkDistanceX(double desiredMovement) {
 
         double r = desiredMovement;
         b.updatePosition(xTopLeft + r, yTopLeft);
@@ -78,18 +85,49 @@ public class Player extends GroundObject implements TimerListener {
         }
 
         if (i < bcs.size()) {
-            while (b.isColliding(bcs.get(i)) ) {
+            while (b.isColliding(bcs.get(i))) {
                 if (moveRight) {
-                    r -= 1.0;
+                    r -= sampleRate;
                     b.updatePosition(xTopLeft + r, yTopLeft);
                 } else if (moveLeft) {
-                    r += 1.0;
+                    r += sampleRate;
                     b.updatePosition(xTopLeft + r, yTopLeft);
                 }
             }
         }
 
         b.updatePosition(xTopLeft + (-1 * r), yTopLeft);
+        return r;
+    }
+
+    double checkDistanceY(double desiredDistance) {
+        double r = desiredDistance;
+        b.updatePosition(xTopLeft, yTopLeft + r);
+
+        int i = 0;
+        for (i = 0; i < bcs.size(); i++) {
+            if (bcs.get(i) == b) {
+                continue;
+            }
+            if (b.isColliding(bcs.get(i))) {
+                break;
+            }
+        }
+
+        if (i < bcs.size()) {
+            while (b.isColliding(bcs.get(i))) {
+                speedY = 0;
+                playerIsJumping = false;
+                if (r > 0) {
+                    r -= sampleRate;
+                } else {
+                    r += sampleRate;
+                }
+                b.updatePosition(xTopLeft, yTopLeft + r);
+            }
+        }
+
+        b.updatePosition(xTopLeft, yTopLeft - r);
         return r;
     }
 
@@ -110,8 +148,25 @@ public class Player extends GroundObject implements TimerListener {
             globe.animMoveAllRel(movement, 0, "Player");
             xTopLeft += movement;
         }
+
+        if (playerIsJumping) {
+            speedY += gravity;
+            if (speedY > 0) {
+                speedY = Math.min(speedY, checkDistanceY(speedY));
+            } else if (speedY < 0) {
+                speedY = Math.max(speedY, checkDistanceY(speedY));
+            }
+            globe.animMoveAllRel(0, speedY, "Player");
+            yTopLeft += speedY;
+        }else{
+            speedY = 0;
+            speedY = Math.max(speedY, checkDistanceY(gravity*5));
+            globe.animMoveAllRel(0, speedY, "Player");
+            yTopLeft += speedY;
+        }
+        
+        
         b.updatePosition(xTopLeft, yTopLeft);
         globe.repaintPanel();
     }
-
 }
