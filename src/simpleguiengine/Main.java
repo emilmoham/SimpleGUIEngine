b86cd9b57b923baac8fd8da783423f1b18d70ddc
Emil <emilmoham@gmail.com>
2015-11-26 23:32:05 -0500

    Changed initial position of player object and visible GroundObjects. Floor is now also a ground object.

--- src/simpleguiengine/Main.java
+++ src/simpleguiengine/Main.java
@@ -9,15 +9,13 @@ public class Main {
         sg.timerStart(15);
         double squareSize = 50;
         
-        OverWorld world = new OverWorld(sg, 50);
-        Player p1 = new Player(sg.getWidth()/20, sg.getHeight() - world.floor - squareSize, squareSize, squareSize, sg);
-        //world.addGroundObject(new GroundObject(5*squareSize, sg.getHeight() - world.floor - squareSize, squareSize, squareSize));
-        world.addPlayer(p1);
-        //world.addGroundObject(new GroundObject(5*squareSize, sg.getHeight() - world.floor - squareSize, squareSize, squareSize));
+        OverWorld world = new OverWorld(sg, 49);
+        Player p1 = new Player(sg.getWidth()/20, sg.getHeight() - world.floor - squareSize - 1, squareSize, squareSize, sg);
+        world.addGroundObject(new GroundObject(5*squareSize, sg.getHeight() - world.floor -  squareSize - 1, squareSize, squareSize));
         world.addGroundObject(new GroundObject(-10, 0, 10, sg.getHeight()));
         world.addGroundObject(new GroundObject(sg.getWidth(),0,10,sg.getHeight()));
+        world.addGroundObject(new GroundObject(0, sg.getHeight() - world.floor, sg.getWidth(), world.floor));
+        world.addPlayer(p1);
         world.visualizeAll();
-        
-        
     }
 }
