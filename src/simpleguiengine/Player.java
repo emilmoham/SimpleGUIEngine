797b153cc76329fdb6ee966fb297d1ef4bd8ced5
Emil <emilmoham@gmail.com>
2015-11-28 18:07:52 -0500

    Key Pressess are now handled with a Listener attached to a JFrame. Jumping is buggy and needs to be tested further

--- src/simpleguiengine/Player.java
+++ src/simpleguiengine/Player.java
@@ -1,73 +1,72 @@
 package simpleguiengine;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.util.ArrayList;
 import simplegui.SimpleGUI;
 import simplegui.TimerListener;
 import simplegui.KeyboardListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
 
 public class Player extends GroundObject implements TimerListener {
+
+    //Control Data
+    ArrayList<Integer> keysDown;
+    JFrame ControlWindow;
+    KeyListener controls = new KeyListener() {
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+            //Unused
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if (!keysDown.contains(e.getKeyCode())) {
+                keysDown.add(new Integer(e.getKeyCode()));
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            keysDown.remove(new Integer(e.getKeyCode()));;
+        }
+    };
 
     //Visualization Data
     SimpleGUI globe;
     //double playerSize = 50;
 
     //Physics and movement data
-    boolean moveLeft = false;
-    boolean moveRight = false;
     boolean playerIsJumping = false;
-    double gravity = 2;
+    boolean playerIsFalling = false;
+    double gravity = 1.0;
     double jumpStrength = -20;
     double speedY = 0;
     double speedX = 5;
     double movement;
-    double sampleRate = 0.1;
+    double sampleRate = 0.5;
 
     public ArrayList<BoxCollider> bcs;
 
-    KeyboardListener controls = new KeyboardListener() {
-
-        @Override
-        public void reactToKeyboardEntry(String string) {
-            //Unused
-        }
-
-        @Override
-        public void reactToKeyboardSingleKey(String string) {
-            //WSAD
-            char c = string.charAt(0);
-            if (c == 'w') {
-                globe.print("");
-                globe.print("UP");
-                if (!playerIsJumping) {
-                    playerIsJumping = true;
-                    speedY = jumpStrength;
-                }
-            }
-            /*
-             if(c == 's'){
-             globe.print("");
-             globe.print("DOWN");
-             }*/
-
-            if (c == 'a') {
-                globe.print("");
-                globe.print("LEFT");
-                moveLeft = true;
-            } else if (c == 'd') {
-                globe.print("");
-                globe.print("RIGHT");
-                moveRight = true;
-            }
-        }
-    };
-
     public Player(double x, double y, double w, double h, SimpleGUI sg) {
         super(x, y, w, h);
-        //playerSize = (w+h)/2;
+        keysDown = new ArrayList();
+        ControlWindow = new JFrame();
+        ControlWindow.addKeyListener(controls);
+        ControlWindow.setFocusable(true);
+        ControlWindow.setFocusTraversalKeysEnabled(false);
+        ControlWindow.setTitle("Control Window");
+        ControlWindow.setSize(500, 50);
+        ControlWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ControlWindow.setVisible(true);
+        ControlWindow.setLocation(new Point(sg.getWidth(), 0));
+
         globe = sg;
-        globe.registerToKeyboard(controls);
         globe.registerToTimer(this);
+
     }
 
     double checkDistanceX(double desiredMovement) {
@@ -86,17 +85,17 @@ public class Player extends GroundObject implements TimerListener {
 
         if (i < bcs.size()) {
             while (b.isColliding(bcs.get(i))) {
-                if (moveRight) {
+                if (keysDown.contains(KeyEvent.VK_D)) {
                     r -= sampleRate;
                     b.updatePosition(xTopLeft + r, yTopLeft);
-                } else if (moveLeft) {
+                } else if (keysDown.contains(KeyEvent.VK_A)) {
                     r += sampleRate;
                     b.updatePosition(xTopLeft + r, yTopLeft);
                 }
             }
         }
 
-        b.updatePosition(xTopLeft + (-1 * r), yTopLeft);
+        b.updatePosition(xTopLeft, yTopLeft);
         return r;
     }
 
@@ -117,17 +116,20 @@ public class Player extends GroundObject implements TimerListener {
         if (i < bcs.size()) {
             while (b.isColliding(bcs.get(i))) {
                 speedY = 0;
-                playerIsJumping = false;
                 if (r > 0) {
                     r -= sampleRate;
+                    //b.updatePosition(xTopLeft, yTopLeft+r);
                 } else {
                     r += sampleRate;
+                    //b.updatePosition(xTopLeft, yTopLeft-r);
                 }
                 b.updatePosition(xTopLeft, yTopLeft + r);
             }
+            playerIsJumping = false;
         }
 
-        b.updatePosition(xTopLeft, yTopLeft - r);
+        b.updatePosition(xTopLeft, yTopLeft);
+        //playerIsJumping = false;
         return r;
     }
 
@@ -137,19 +139,31 @@ public class Player extends GroundObject implements TimerListener {
 
     @Override
     public void reactToTimer(long l) {
-        if (moveRight) {
+        if (keysDown.contains(KeyEvent.VK_D)) {
             movement = Math.min(speedX, checkDistanceX(speedX));
-            moveRight = false;
             globe.animMoveAllRel(movement, 0, "Player");
             xTopLeft += movement;
-        } else if (moveLeft) {
+            //System.out.println("Player:   " + xTopLeft + " " + yTopLeft);
+            //System.out.println("Collider: " + b.xTopLeft+ " " + b.yTopLeft);
+        } else if (keysDown.contains(KeyEvent.VK_A)) {
             movement = Math.max(-1 * speedX, checkDistanceX(-1 * speedX));
-            moveLeft = false;
             globe.animMoveAllRel(movement, 0, "Player");
             xTopLeft += movement;
+            //System.out.println("Player:   " + xTopLeft + " " + yTopLeft);
+            //System.out.println("Collider: " + b.xTopLeft+ " " + b.yTopLeft);
         }
 
-        if (playerIsJumping) {
+        if (!playerIsJumping) {
+            if (keysDown.contains(KeyEvent.VK_W)) {
+                playerIsJumping = true;
+                speedY = jumpStrength;
+            } else {
+                speedY += gravity;
+                speedY = Math.min(speedY, checkDistanceY(speedY));
+                globe.animMoveAllRel(0, speedY, "Player");
+                yTopLeft += speedY;
+            }
+        } else {
             speedY += gravity;
             if (speedY > 0) {
                 speedY = Math.min(speedY, checkDistanceY(speedY));
@@ -158,14 +172,9 @@ public class Player extends GroundObject implements TimerListener {
             }
             globe.animMoveAllRel(0, speedY, "Player");
             yTopLeft += speedY;
-        }else{
-            speedY = 0;
-            speedY = Math.max(speedY, checkDistanceY(gravity*5));
-            globe.animMoveAllRel(0, speedY, "Player");
-            yTopLeft += speedY;
+
         }
-        
-        
+
         b.updatePosition(xTopLeft, yTopLeft);
         globe.repaintPanel();
     }
