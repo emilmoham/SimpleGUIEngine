26af66bee4ca44d0d84090ea9692fc011aeb6890
Emil <emilmoham@gmail.com>
2015-11-28 18:08:40 -0500

    Added A few extra GroundObjects for testing purposes.

--- src/simpleguiengine/Main.java
+++ src/simpleguiengine/Main.java
@@ -1,8 +1,9 @@
 package simpleguiengine;
+
 import simplegui.SimpleGUI;
 
 public class Main {
-
+    
     public static void main(String[] args) {
         SimpleGUI sg = new SimpleGUI();
         
@@ -10,12 +11,14 @@ public class Main {
         double squareSize = 50;
         
         OverWorld world = new OverWorld(sg, 49);
-        Player p1 = new Player(sg.getWidth()/20, sg.getHeight() - world.floor - squareSize - 1, squareSize, squareSize, sg);
-        world.addGroundObject(new GroundObject(5*squareSize, sg.getHeight() - world.floor -  squareSize - 1, squareSize, squareSize));
+        Player p1 = new Player(sg.getWidth() / 20, sg.getHeight() - world.floor - squareSize - 1, squareSize, squareSize, sg);
+        world.addGroundObject(new GroundObject(5 * squareSize, sg.getHeight() - world.floor - squareSize - 1, squareSize, squareSize));
+        world.addGroundObject(new GroundObject(5 * squareSize + 20, sg.getHeight() - 3 * world.floor - 20, squareSize, squareSize));
         world.addGroundObject(new GroundObject(-10, 0, 10, sg.getHeight()));
-        world.addGroundObject(new GroundObject(sg.getWidth(),0,10,sg.getHeight()));
+        world.addGroundObject(new GroundObject(sg.getWidth(), 0, 10, sg.getHeight()));
         world.addGroundObject(new GroundObject(0, sg.getHeight() - world.floor, sg.getWidth(), world.floor));
         world.addPlayer(p1);
         world.visualizeAll();
+        p1.ControlWindow.requestFocus();
     }
 }
